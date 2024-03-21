86b9ff15356c3131052a25c2fb1d87299ce4ea39
Tony Alfaro <26556488+pythonyalfa@users.noreply.github.com>
2024-03-20 22:05:55 -0600

    Delete APIs_REACT directory

--- APIs_REACT/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Todo
-# Register your models here.
-
-class displayTodo(admin.ModelAdmin):
-    list_display = ('title', 'title','completed', 'user')  # Add the fields you want to display in the list view
-
-
-admin.site.register(Todo, displayTodo)
